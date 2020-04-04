14f2bf0ddba1762d379dd67e39d9a1ed8a2407bb
Ashish Bhatia <ashishbhatia8989@gmail.com>
2020-04-04 10:52:05 -0700

    Add project files.

---
+++ QueueImplementation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace QueueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the circular Queue");
+            Queue queue = new Queue();
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\abhatia\source\repos\QueueImplementation\QueueImplementation\TextFile1.txt");
+            string line;
+            int counter = 0;
+            int dequeudint = 0;
+
+            try
+            {
+
+                while ((line = file.ReadLine()) != null)
+                {
+
+                    Console.WriteLine(line);
+                    queue.Enqueue(int.Parse(line));
+
+                }
+
+
+                Console.WriteLine("Starting to Read Queue");
+                int[] copyofqueue = queue.ReturnQueue();
+                for (int entry = 0; entry < copyofqueue.Length; entry++)
+                {
+                    Console.WriteLine(copyofqueue[entry].ToString());
+                }
+
+
+
+                Console.WriteLine("Starting DeqUeue");
+                counter = 0;
+                do
+                {
+                    dequeudint = queue.DeQueue();
+                    Console.WriteLine(dequeudint.ToString());
+                    counter++;
+                }
+                while (counter <= copyofqueue.Length);
+
+                               
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                file.Close();
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
---
+++ QueueImplementation/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace QueueImplementation
+{
+    class Queue
+    {
+        private const int DefaultQueueSize = 10;
+        private int[] data;
+        private int head = 0, tail = 0;
+        private int numElements = 0;
+
+        
+
+
+        public Queue()
+        {
+            this.data = new int[DefaultQueueSize];
+        }
+
+        public Queue(int size)
+        {
+            if (size > 0)
+            {
+                this.data = new int[size];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("size", " must be great than zero");
+            }
+        }
+
+        public void Enqueue(int item)
+        {
+            if (this.numElements == data.Length)
+            {
+                throw new Exception("Queue full");
+            }
+            this.data[this.head] = item;
+            this.head++;
+            this.head = this.head % this.data.Length;
+            this.numElements++;
+        }
+
+        public int[] ReturnQueue()
+        {
+            return this.data;
+        }
+
+        public int DeQueue()
+        {
+            if (this.numElements == 0)
+            {
+                throw new Exception("Queue Empty");
+            }
+
+            int queueitem = this.data[this.tail];
+            this.tail++;
+            //this.tail %= this.tail % this.data.Length;
+            this.numElements--;
+            return queueitem;
+        }
+    }
+}
